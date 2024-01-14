5dea8ec988ef473c4115f7ffc1dde63cb1c49ef3
XingYu-Zhong <1736101137@qq.com>
2024-01-14 11:33:39 +0800

    add proxy

--- run.py
+++ run.py
@@ -9,10 +9,12 @@ parser.add_argument("-et", "--env_type", required=True, help="env_type 训练类
 parser.add_argument("-s", "--start_time", required=True, help="交易开始时间")
 parser.add_argument("-e", "--end_time", required=True, help="交易结束时间")
 parser.add_argument("-lt", "--load_time_steps", required=False, help="继续训练观察数")
+parser.add_argument("-p", "--proxy", required=False, help="代理端口")
 args = parser.parse_args()
-# 设置代理
-os.environ['HTTP_PROXY'] = 'http://127.0.0.1:10809'
-os.environ['HTTPS_PROXY'] = 'http://127.0.0.1:10809'
+if args.proxy:
+    # 设置代理
+    os.environ['HTTP_PROXY'] = 'http://127.0.0.1:'+args.proxy
+    os.environ['HTTPS_PROXY'] = 'http://127.0.0.1:'+args.proxy
 if args.env_type=="load":
     MainLab(args.task_name,args.env_type,args.start_time,args.end_time,int(float(args.load_time_steps)))
 else:
